710477c035ed5378b213ed78db27f70db3704b34
mateusbds2 <mateusbds2@hotmail.com>
2015-03-16 13:30:10 -0300

    Create SDLGameObject.cpp

---
+++ SDLGameObject.cpp
@@ -0,0 +1,41 @@
+#include "SDLGameObject.h"
+#include "TextureManager.h"
+#include "Game.h"
+
+//É preciso iniciar todos os consts e statics no construtor
+
+SDLGameObject::SDLGameObject(const LoaderParams* pParams) : GameObject(pParams), m_position(pParams->getX(), pParams->getY()), m_acceleration(0, 0), m_velocity(0,0){
+	m_x = pParams->getX();
+	m_y = pParams->getY();
+	m_width = pParams->getWidth();
+	m_height = pParams->getHeight();
+	m_textureID = pParams->getTextureID();
+	face = 1;
+	m_currentRow = 1;
+	m_currentFrame = 1;
+}
+
+void SDLGameObject::update(){
+	m_position += m_velocity;
+}
+
+void SDLGameObject::draw(){
+	if (m_velocity.getX() < 0 || face == -1)
+	{
+		TextureManager::Instance()->drawFrame(m_textureID,
+			(Uint32)m_position.getX(), (Uint32)m_position.getY(),
+			m_width, m_height, m_currentRow, m_currentFrame,
+			TheGame::Instance()->getRenderer(), SDL_FLIP_HORIZONTAL);
+	}
+	else
+	{
+		TextureManager::Instance()->drawFrame(m_textureID,
+			(Uint32)m_position.getX(), (Uint32)m_position.getY(),
+			m_width, m_height, m_currentRow, m_currentFrame,
+			TheGame::Instance()->getRenderer());
+	}
+}
+
+void SDLGameObject::clean(){
+
+}
