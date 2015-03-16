0756f97d775d894eb826feb0f52d481ed1331ac1
mateusbds2 <mateusbds2@hotmail.com>
2015-03-16 13:37:45 -0300

    Create Enemy.cpp

---
+++ Enemy.cpp
@@ -0,0 +1,19 @@
+#include "Enemy.h"
+#include "TextureManager.h"
+#include "Game.h"
+
+Enemy::Enemy(const LoaderParams* pParams) : SDLGameObject(pParams){
+
+}
+
+void Enemy::update(){
+	
+}
+
+void Enemy::draw(){
+	SDLGameObject::draw();
+}
+
+void Enemy::clean(){
+
+}
